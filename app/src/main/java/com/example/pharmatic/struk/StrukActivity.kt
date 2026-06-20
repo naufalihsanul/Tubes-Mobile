9b4cb83bf2b9866f1c20b810dd7020ba45cca0e9
Naufal72k <naufal72k@gmail.com>
2026-06-20 19:41:23 +0800

    gass

--- app/src/main/java/com/example/pharmatic/struk/StrukActivity.kt
+++ app/src/main/java/com/example/pharmatic/struk/StrukActivity.kt
@@ -8,7 +8,6 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.pharmatic.MainActivity
 import com.example.pharmatic.R
 import com.example.pharmatic.keranjang.KeranjangManager
 import com.example.pharmatic.model.Transaksi
